9414af54c952266a26a47aa2e13411a309eb16c9
derek <lideyu888@163.com>
2020-08-06 14:13:20 +0800

    feat:load方法新增队列加载

--- src/load.js
+++ src/load.js
@@ -1,22 +1,51 @@
 import Loader from './loaders/loader';
 
-/**
- * load
- * @param loaders
- * @returns {Promise<any[]>}
- */
-function load(loaders = []) {
+function queueLoad(loaders) {
+  let promise = Promise.resolve([]);
+
+  for (let i = 0, len = loaders.length; i < len; i++) {
+    const loader = loaders[i];
+    promise = promise.then(result => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          if (loader instanceof Loader) {
+            result.push(await loader.load());
+          } else {
+            result.push(await loader);
+          }
+          resolve(result);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  }
+
+  return promise;
+}
+
+function parallelLoad(loaders) {
   const promiseList = [];
 
-  loaders.forEach(loader => {
+  for (let i = 0, len = loaders.length; i < len; i++) {
+    const loader = loaders[i];
     if (loader instanceof Loader) {
       promiseList.push(loader.load());
     } else {
       promiseList.push(loader);
     }
-  });
+  }
 
   return Promise.all(promiseList);
+}
+
+/**
+ * load
+ * @param loaders
+ * @returns {Promise<any[]>}
+ */
+function load(loaders = [], queue = true) {
+  return queue ? queueLoad(loaders) : parallelLoad(loaders);
 }
 
 export default load;
