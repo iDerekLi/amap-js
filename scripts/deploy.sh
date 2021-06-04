558929efdbab14669e2c48892891929195952aad
derek <lideyu888@163.com>
2021-06-04 15:09:34 +0800

    ci: 更新脚本

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,22 +5,34 @@ AMapJSVersion="v2"
 cross-env yarn docs:build
 cross-env v=$AMapJSVersion yarn docs:build
 
+# Pull remote code
 mkdir temp_web
 cd temp_web
-
 git clone --depth 1 -b gh-pages --single-branch https://github.com/iDerekLi/amap-js.git
 cd amap-js
 
+# Merge code
 rm -rf *.html *.js *.css *.map assets guide
 rm -rf $AMapJSVersion/**
 cp -rf ../../docs/.vuepress/dist/last/** .
 mkdir $AMapJSVersion
 cp -rf  ../../docs/.vuepress/dist/$AMapJSVersion/** $AMapJSVersion/
-git add -A .
-git commit -m "deploy"
-git push origin gh-pages
-cd ../../
 
+# Push origin
+if [ -n "$(git status -s)" ];then
+  git config user.name "Derek Li"
+  git config user.email "lideyu888@163.com"
+  git add -A .
+  git commit -m "deploy"
+  git push origin gh-pages
+else
+  echo "nothing to commit, working tree clean"
+fi
+
+# Clean Temp
+cd ../../
 rm -rf temp_web
+
+# Done
 echo "DONE, Bye~"
 exit 0
