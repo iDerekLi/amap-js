00eae76b5b84ef6200b8baee3cc7d4b86d922fb6
Derek Li <lideyu888@163.com>
2021-12-07 14:58:36 +0800

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,21 +2,21 @@
 AMapJSVersion="v2"
 
 # build dev site
-#cross-env yarn docs:build
-#cross-env v=$AMapJSVersion yarn docs:build
+cross-env yarn docs:build
+cross-env v=$AMapJSVersion yarn docs:build
 
 # Pull remote code
-#mkdir temp_web
+mkdir temp_web
 cd temp_web
-#git clone --depth 1 -b gh-pages --single-branch https://github.com/iDerekLi/amap-js.git
+git clone --depth 1 -b gh-pages --single-branch https://github.com/iDerekLi/amap-js.git
 cd amap-js
 
 # Merge code
-#rm -rf *.html *.js *.css *.map assets guide
-#rm -rf $AMapJSVersion/**
-#cp -rf ../../docs/.vuepress/dist/last/** .
-#mkdir $AMapJSVersion
-#cp -rf  ../../docs/.vuepress/dist/$AMapJSVersion/** $AMapJSVersion/
+rm -rf *.html *.js *.css *.map assets guide
+rm -rf $AMapJSVersion
+cp -rf ../../docs/.vuepress/dist/last/** .
+mkdir $AMapJSVersion
+cp -rf  ../../docs/.vuepress/dist/$AMapJSVersion/** $AMapJSVersion/
 
 # Push origin
 if [ -n "$(git status -s)" ];then
